140abe3b494b21fb2264dccb8301c42e21ce297d
Théo FORTIN <theo.fortin1@gmail.com>
2020-09-30 16:39:58 +0200

    workerFetch

--- worker.js
+++ worker.js
@@ -1,12 +1,29 @@
 const cacheName = "static";
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
     event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-    return cache.addAll(
-    [
-    'index.html'
-    ]
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(
+                [
+                    'index.html'
+                ]
+            );
+        })
     );
-    })
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(even, request)
+            .then(function (response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event, request)
+                    .then(function (cache) {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    })
+
+            })
     );
-   }); 
+}); 
