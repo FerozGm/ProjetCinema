82c62464e63060b04125a192c7873e1913385fd3
Théo FORTIN <theo.fortin1@gmail.com>
2020-09-30 16:21:00 +0200

    cache

---
+++ worker.js
@@ -0,0 +1,12 @@
+const cacheName = "static";
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+    return cache.addAll(
+    [
+    'index.html'
+    ]
+    );
+    })
+    );
+   }); 
